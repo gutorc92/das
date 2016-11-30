38f6e43a5908b1b6435618e8afe303e7bf2afc98
Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
2016-11-30 18:26:16 -0200

    Adding categories at the gallery page
    Signed-off-by: Elaine Cristina Meirelles <ecmeirelles@hotmail.com>
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

--- das/gallery/views.py
+++ das/gallery/views.py
@@ -5,14 +5,8 @@ from .forms import FileFieldForm
 from django.conf import settings
 from extern import *
 import os
-from os import walk
 
 path_img = os.path.join(settings.BASE_DIR, "images")
-
-def handle_uploaded_file(f, name):
-    with open(name, 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
 
 class CreateGallery(View):
 
@@ -21,18 +15,18 @@ class CreateGallery(View):
         form = FileFieldForm(request.POST, request.FILES)
         if form.is_valid():
             return self.get(request)
-
         else:
             return render(request, "gallery/create.html",{'form': form})
-     
+
     def get(self, request):
         form = FileFieldForm()
-        return render(request, "gallery/create.html",{'form': form})
-        
+        create_categories()
+        categories = Category.objects.all()
+        return render(request, "gallery/create.html",{'form': form, 'categories':categories})
+
 
 def create_categories():
    cats,  created = Category.objects.get_or_create(name="Cats")
    dogs,  created = Category.objects.get_or_create(name="Dogs")
    faces, created = Category.objects.get_or_create(name="Faces")
    return cats, dogs, faces
-# Create your views here.
